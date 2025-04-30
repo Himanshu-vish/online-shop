0c7bb57cec4fd901ff5e33cbc9b240efa898c82b
himanshu-vish <himanshu.in2021@gmail.com>
2025-04-30 10:16:02 +0530

    Auth Controller Updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
+
 class AuthController extends Controller
 {
     public function register(){
